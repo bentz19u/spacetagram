88f7a4912ebb44f9acf5ff7e226b57220bbf9792
Daniel Bentz <db.daniel.bentz@gmail.com>
2024-11-23 09:00:59 +0900

    redirected github link to the repository directly

--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
     <header className='flex h-14 justify-center bg-neutral-900'>
       <div id='header-content' className='flex justify-between text-center'>
         <div className={`${cookie.className} text-3xl`}>Spacetagram</div>
-        <Link href='https://github.com/bentz19u'>
+        <Link href='https://github.com/bentz19u/spacetagram' target='_blank'>
           <Image
             src='/github-mark-white.svg'
             alt='Git hub logo'
