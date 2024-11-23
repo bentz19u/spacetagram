607839017bc1c070abe00e9785b3662a0b506d2a
Daniel Bentz <db.daniel.bentz@gmail.com>
2024-11-23 15:17:53 +0900

    added Linkedin url and logo

--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -9,14 +9,27 @@ export default function Header() {
     <header className='flex h-14 justify-center bg-neutral-900'>
       <div id='header-content' className='flex justify-between text-center'>
         <div className={`${cookie.className} text-3xl`}>Spacetagram</div>
-        <Link href='https://github.com/bentz19u/spacetagram' target='_blank'>
-          <Image
-            src='/github-mark-white.svg'
-            alt='Git hub logo'
-            width='25'
-            height='25'
-          ></Image>
-        </Link>
+        <div id='header-logo' className='flex justify-center text-center'>
+          <Link
+            href='https://www.linkedin.com/in/daniel-bentz-1682b137/'
+            target='_blank'
+          >
+            <Image
+              src='/LI-In-Bug.png'
+              alt='Linkedin logo'
+              width='30'
+              height='30'
+            ></Image>
+          </Link>
+          <Link href='https://github.com/bentz19u/spacetagram' target='_blank'>
+            <Image
+              src='/github-mark-white.svg'
+              alt='Git hub logo'
+              width='25'
+              height='25'
+            ></Image>
+          </Link>
+        </div>
       </div>
     </header>
   );
